3d62cf7ea8b887de24372a03949427db1cd44d49
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-01-08 18:20:51 +0300

    правки

--- js/canvas.js
+++ js/canvas.js
@@ -29,7 +29,7 @@ class Bubble {
   init() {
     this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
     this.alpha = generateDecimalBetween(1, 10) / 10;
-    this.size = generateDecimalBetween(0.2, 2);
+    this.size = generateDecimalBetween(0.2, 1);
     this.translateX = generateDecimalBetween(0, this.canvasWidth);
     this.translateY = generateDecimalBetween(0, this.canvasHeight);
 
