5de19e0439a80b0d34694b1565008c32cc75a90a
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-01-19 21:49:28 +0300

    исправление анимации

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,23 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+let animationInited = false;
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+  }
+
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector(
+    '.features__clients-count'
+  ).offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+window.addEventListener('scroll', updateScroll);
 
 function increaseNumberAnimationStep(i, element, endNumber) {
   if (i <= endNumber) {
@@ -19,7 +38,6 @@ function initIncreaseNumberAnimation() {
   let element = document.querySelector('.features__clients-count');
   increaseNumberAnimationStep(0, element, 5000);
 }
-initIncreaseNumberAnimation();
 
 document
   .querySelector('#budget')
